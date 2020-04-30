c60680728ebed50b002f9a01b2e56063ae9366c4
Mark Wilde <markwilde29@gmail.com>
2020-04-30 15:25:33 +0100

    Completed tutorial challenge 1.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-ReactDom.render(<h1>Hello Clarice</h1>, document.getElementById("root"));
+// ReactDom.render(What do I want to render, where do I want to render it);
+ReactDom.render(
+  <div>
+    <ul>
+      <li>hello</li>
+      <li>hoo hah</li>
+      <li>boo boo</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
