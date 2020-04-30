b400660adda0a13a4f3fa91c1213467eb80ad69f
Mark Wilde <markwilde29@gmail.com>
2020-04-30 15:59:18 +0100

    Completed tutorial challenge 2.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDom from "react-dom";
+import "./style.css";
+
+// functional component(s)
+function MyInfo() {
+  return (
+    <div className="DottedBox">
+      <div className="DottedBox_content">
+        <h1>Mark</h1>
+        <p>
+          <strong>Likes coding, doesn't like watching TV</strong>
+        </p>
+        <h3>Here are some places I would like to visit</h3>
+        <ul>
+          <li>Almeria</li>
+          <li>San Francisco</li>
+          <li>Vienna</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 // ReactDom.render(What do I want to render, where do I want to render it);
-ReactDom.render(
-  <div>
-    <ul>
-      <li>hello</li>
-      <li>hoo hah</li>
-      <li>boo boo</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDom.render(<MyInfo />, document.getElementById("root"));
