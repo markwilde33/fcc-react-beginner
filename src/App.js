a7f296f52fcba60139d4a47a9c277e97a386ab32
Mark Wilde <markwilde29@gmail.com>
2020-05-01 11:02:11 +0100

    Added a new component. Outputted each card in ContactCard.js in App.js by using prop. Added some styles to cards.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,35 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import TodoItem from "./components/TodoItem";
+import ContactCard from "./components/ContactCard";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <TodoItem />
-      <Footer />
+    <div className="contacts">
+      <ContactCard
+        name="Bill"
+        imgUrl="http://placekitten.com/400/298"
+        phone="018490456"
+        email="bill@gmail.com"
+      />
+      <ContactCard
+        name="Bob"
+        imgUrl="http://placekitten.com/400/300"
+        phone="018490455"
+        email="bob@gmail.com"
+      />
+      <ContactCard
+        name="Berry"
+        imgUrl="http://placekitten.com/400/301"
+        phone="018490450"
+        email="berry@gmail.com"
+      />
+      <ContactCard
+        name="Baba"
+        imgUrl="http://placekitten.com/400/297"
+        phone="018490451"
+        email="baba@gmail.com"
+      />
     </div>
   );
 }
