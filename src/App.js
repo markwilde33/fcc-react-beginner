bf868809dc030bcbe24c0400045d65728f2861a4
Mark Wilde <markwilde29@gmail.com>
2020-05-01 16:15:29 +0100

    Converted fromm a function based component, to a class based component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
-import TodoItem from "./components/TodoItem";
-import todosData from "./components/todosData";
 
-function App() {
-  const todoItems = todosData.map((todo) => (
-    <TodoItem key={todo.id} todo={todo} />
-  ));
-  return <div className="todo-list">{todoItems}</div>;
+// function App(props) {
+//     return (
+//         <div>
+//             <h1>{props.whatever}</h1>
+//         </div>
+//     )
+// }
+
+class App extends React.Component {
+  yourMethodHere() {}
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.whatever}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
