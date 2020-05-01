8c336049b318928b38a7379aafdad10116cae429
Mark Wilde <markwilde29@gmail.com>
2020-05-01 09:45:02 +0100

    Added a new component -- TodoItem.js.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <TodoItem />
       <Footer />
     </div>
   );
---
+++ src/components/TodoItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TodoItem() {
+  return (
+    <div>
+      <input type="checkbox" />
+      <p className="todo-par">Placeholder text here</p>
+      <input type="checkbox" />
+      <p className="todo-par">Placeholder text here</p>
+      <input type="checkbox" />
+      <p className="todo-par">Placeholder text here</p>
+      <input type="checkbox" />
+      <p className="todo-par">Placeholder text here</p>
+    </div>
+  );
+}
+
+export default TodoItem;
