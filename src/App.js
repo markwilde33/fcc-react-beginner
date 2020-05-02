a9ad2bc201d93664b8c69df57e5643e9a6ed8a57
Mark Wilde <markwilde29@gmail.com>
2020-05-02 10:06:40 +0100

    Completed challenge 8.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      answer: "Yes",
+      name: "Clarice",
+      age: 29,
     };
   }
 
   render() {
     return (
       <div>
-        <h1>Is state important to know? {this.state.answer}</h1>
+        <h1>Hi my name is {this.state.name}</h1>
+        <h3> When I first met Hannibal I was {this.state.age} years old</h3>
       </div>
     );
   }
