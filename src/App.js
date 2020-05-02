096328d9d9843f1760e6ed7a8be3452380ba6a42
Mark Wilde <markwilde29@gmail.com>
2020-05-02 10:25:00 +0100

    Completed challenge 9.

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      name: "Clarice",
-      age: 29,
+      isLoggedIn: true,
     };
   }
 
   render() {
     return (
       <div>
-        <h1>Hi my name is {this.state.name}</h1>
-        <h3> When I first met Hannibal I was {this.state.age} years old</h3>
+        <h1>
+          The user is logged {this.state.isLoggedIn === true ? "in" : "out"}
+        </h1>
       </div>
     );
   }
