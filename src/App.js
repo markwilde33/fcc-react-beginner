c0d0adf36d84eff6c3c87a24dce17ef8ead0f0f2
Mark Wilde <markwilde29@gmail.com>
2020-05-03 14:34:41 +0100

    Lifecycles methods part one.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import React from "react";
-import TodoItem from "./components/TodoItem";
-import todosData from "./components/todosData";
+import React, { Component } from "react";
 
-class App extends React.Component {
+// https://engineering.musefind.com/react-lifecycle-methods-how-and-when-to-use-them-2111a1b692b1
+// https://reactjs.org/blog/2018/03/29/react-v-16-3.html#component-lifecycle-changes
+
+class TodoList extends Component {
   constructor() {
     super();
-    this.state = {
-      todos: todosData,
-    };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {};
   }
 
-  handleChange(id) {
-    this.setState((prevState) => {
-      const updatedTodos = prevState.todos.map((todo) => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      });
-      return {
-        todos: updatedTodos,
-      };
-    });
+  componentDidMount() {
+    // GET the data I need to correctly display
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.whatever !== this.props.whatever) {
+      // do something important here
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // return true if want it to update
+    // return false if not
+  }
+
+  componentWillUnmount() {
+    // teardown or cleanup your code before your component disappears
+    // (E.g. remove event listeners)
   }
 
   render() {
-    const todoItems = this.state.todos.map((item) => (
-      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
-    ));
-
-    return <div className="todo-list">{todoItems}</div>;
+    return <div>Code goes here</div>;
   }
 }
 
