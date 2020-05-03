47e9cc128bac7e89443d31f393612974ed8aa59b
Mark Wilde <markwilde29@gmail.com>
2020-05-03 14:20:19 +0100

    Gave the solution to challenge 12

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-// working with (changing) states in react
-
 import React from "react";
+import TodoItem from "./components/TodoItem";
+import todosData from "./components/todosData";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      count: 0,
+      todos: todosData,
     };
-    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleClick() {
+  handleChange(id) {
     this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
       return {
-        count: prevState.count + 1,
+        todos: updatedTodos,
       };
     });
   }
 
   render() {
-    return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.handleClick}>Change</button>
-      </div>
-    );
+    const todoItems = this.state.todos.map((item) => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+
+    return <div className="todo-list">{todoItems}</div>;
   }
 }
 
