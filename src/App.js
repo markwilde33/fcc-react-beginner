c5fe32d0ac5577c2ba3bb374a3de1c7919e455a7
Mark Wilde <markwilde29@gmail.com>
2020-05-04 15:27:15 +0100

    Provided examples of other useful and common place form elements, and how they are used and written in React.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,116 @@
 import React, { Component } from "react";
 
+{
+  /**
+   * Other useful form elements:
+   *
+   *  <textarea /> element
+   *  <input type="checkbox" />
+   *  <input type="radio" />
+   *  <select> and <option> elements
+   */
+}
+{
+  /* Useful website for building forms in React (without the tears):
+     https://jaredpalmer.com/formik/
+  */
+}
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
       firstName: "",
       lastName: "",
+      isFriendly: false,
+      gender: "",
+      favColor: "blue",
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value,
-    });
+    const { name, value, type, checked } = event.target;
+    type === "checkbox"
+      ? this.setState({ [name]: checked })
+      : this.setState({ [name]: value });
   }
 
   render() {
     return (
-      <div>
-        <h2>Code goes here</h2>
-        <form>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.firstName}
+          name="firstName"
+          placeholder="First Name"
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="text"
+          value={this.state.lastName}
+          name="lastName"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+        />
+
+        <textarea value={"Some default value"} onChange={this.handleChange} />
+
+        <br />
+
+        <label>
           <input
-            type="text"
-            value={this.state.firstName}
-            name="firstName"
-            placeholder="First Name"
+            type="checkbox"
+            name="isFriendly"
+            checked={this.state.isFriendly}
             onChange={this.handleChange}
-          />
+          />{" "}
+          Is friendly?
+        </label>
+        <br />
+        <label>
           <input
-            type="text"
-            value={this.state.lastName}
-            name="lastName"
-            placeholder="Last Name"
+            type="radio"
+            name="gender"
+            value="male"
+            checked={this.state.gender === "male"}
             onChange={this.handleChange}
-          />
-          <h2>
-            {this.state.firstName} {this.state.lastName}
-          </h2>
-        </form>
-      </div>
+          />{" "}
+          Male
+        </label>
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={this.state.gender === "female"}
+            onChange={this.handleChange}
+          />{" "}
+          Female
+        </label>
+        <br />
+        <label>Favorite Color:</label>
+        <select
+          value={this.state.favColor}
+          onChange={this.handleChange}
+          name="favColor"
+        >
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+        </select>
+
+        <h1>
+          {this.state.firstName} {this.state.lastName}
+        </h1>
+        <h2>You are a {this.state.gender}</h2>
+        <h2>Your favorite color is {this.state.favColor}</h2>
+        <button>Submit</button>
+      </form>
     );
   }
 }
