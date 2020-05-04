c39bf61978c47c7439f2bac7aff5f9b34befd283
Mark Wilde <markwilde29@gmail.com>
2020-05-04 15:37:40 +0100

    Setup for challenge 15.

--- src/App.js
+++ src/App.js
@@ -1,116 +1,42 @@
-import React, { Component } from "react";
-
-{
-  /**
-   * Other useful form elements:
-   *
-   *  <textarea /> element
-   *  <input type="checkbox" />
-   *  <input type="radio" />
-   *  <select> and <option> elements
-   */
-}
-{
-  /* Useful website for building forms in React (without the tears):
-     https://jaredpalmer.com/formik/
-  */
-}
-
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      firstName: "",
-      lastName: "",
-      isFriendly: false,
-      gender: "",
-      favColor: "blue",
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    const { name, value, type, checked } = event.target;
-    type === "checkbox"
-      ? this.setState({ [name]: checked })
-      : this.setState({ [name]: value });
+    this.state = {};
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.firstName}
-          name="firstName"
-          placeholder="First Name"
-          onChange={this.handleChange}
-        />
-        <br />
-        <input
-          type="text"
-          value={this.state.lastName}
-          name="lastName"
-          placeholder="Last Name"
-          onChange={this.handleChange}
-        />
+      <main>
+        <form>
+          <input placeholder="First Name" />
+          <br />
+          <input placeholder="Last Name" />
+          <br />
+          <input placeholder="Age" />
+          <br />
 
-        <textarea value={"Some default value"} onChange={this.handleChange} />
+          {/* Create radio buttons for gender here */}
+          <br />
 
-        <br />
+          {/* Create select box for location here */}
+          <br />
 
-        <label>
-          <input
-            type="checkbox"
-            name="isFriendly"
-            checked={this.state.isFriendly}
-            onChange={this.handleChange}
-          />{" "}
-          Is friendly?
-        </label>
-        <br />
-        <label>
-          <input
-            type="radio"
-            name="gender"
-            value="male"
-            checked={this.state.gender === "male"}
-            onChange={this.handleChange}
-          />{" "}
-          Male
-        </label>
-        <br />
-        <label>
-          <input
-            type="radio"
-            name="gender"
-            value="female"
-            checked={this.state.gender === "female"}
-            onChange={this.handleChange}
-          />{" "}
-          Female
-        </label>
-        <br />
-        <label>Favorite Color:</label>
-        <select
-          value={this.state.favColor}
-          onChange={this.handleChange}
-          name="favColor"
-        >
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-          <option value="red">Red</option>
-          <option value="orange">Orange</option>
-          <option value="yellow">Yellow</option>
-        </select>
+          {/* Create check boxes for dietary restrictions here */}
+          <br />
 
-        <h1>
-          {this.state.firstName} {this.state.lastName}
-        </h1>
-        <h2>You are a {this.state.gender}</h2>
-        <h2>Your favorite color is {this.state.favColor}</h2>
-        <button>Submit</button>
-      </form>
+          <button>Submit</button>
+        </form>
+        <hr />
+        <h2>Entered information:</h2>
+        <p>Your name: {/* First and last name here */}</p>
+        <p>Your age: {/* Age here */}</p>
+        <p>Your gender: {/* Gender here */}</p>
+        <p>Your destination: {/* Destination here */}</p>
+        <p>
+          Your dietary restrictions:
+          {/* Dietary restrictions here, comma separated */}
+        </p>
+      </main>
     );
   }
 }
