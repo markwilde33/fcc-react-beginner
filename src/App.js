b6c4cae74b418f50e017dbe8d2449b1e6126f447
Mark Wilde <markwilde29@gmail.com>
2020-05-05 17:08:57 +0100

    This class component using state has been replaced with a functional component using the useState hook.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            answer: "Yes"
-        }
-    }
-    
-    render() {
-        return (
-            <div>
-                <h1>Is state important to know? {this.state.answer}</h1>
-            </div>
-        )
-    }
+function App() {
+    const [answer] = useState("Yes")
+    return (
+        <div>
+            <h1>Is state important to know? {answer}</h1>
+        </div>
+    )
 }
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             answer: "Yes"
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Is state important to know? {this.state.answer}</h1>
+//             </div>
+//         )
+//     }
+// }
 
 export default App
